fb1baabf26a3e04758422df1f8ab9df13cf03c91
ali sali <salimian.ali98@gmail.com>
2022-08-30 22:44:50 +0430

    add requirements.txt and organized imports

--- solve_audio_recaptcha.py
+++ solve_audio_recaptcha.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager as cdm
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import requests
-import requests_random_user_agent
+from webdriver_manager.chrome import ChromeDriverManager as cdm
 import speech_recognition as sr
 from pydub import AudioSegment
+import time
+import requests
 
 
 def main():
